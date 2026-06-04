1fc616f117afd5755ea1fca9f0c8383e672c8d74
Mario Lorenzo <mariolorenzo1976@gmail.com>
2026-06-04 21:03:45 +0100

    Añadimos cambios a la section 3 del portfolio

--- HTML/19_Mi_primer_portafolio/script.js
+++ HTML/19_Mi_primer_portafolio/script.js
@@ -1,19 +1,39 @@
-document.querySelectorAll('.texto-completo').forEach(bloque => {
+// document.querySelectorAll('.texto-completo').forEach(bloque => {
 
-    let hideText_btn = bloque.querySelector('.readMore_btn');
-    let hideText = bloque.querySelector('.hideText');
+//     let hideText_btn = bloque.querySelector('.readMore_btn');
+//     let hideText = bloque.querySelector('.hideText');
 
-    hideText_btn.addEventListener('click', () => {
+//     hideText_btn.addEventListener('click', () => {
+
+//         hideText.classList.toggle('show');
+
+//         if(hideText.classList.contains('show')){
+//             hideText_btn.innerHTML = 'Leer menos...';
+//         }
+//         else{
+//             hideText_btn.innerHTML = 'Leer más...';
+//         }
+
+//     });
+   
+    
+// });
+
+document.querySelectorAll('.readMore_btn').forEach(btn => {
+
+    btn.addEventListener('click', () => {
+
+        const bloque = btn.closest('.texto-completo');
+
+        const hideText = bloque.querySelector('.hideText');
+
+        if (!hideText) return;
 
         hideText.classList.toggle('show');
 
-        if(hideText.classList.contains('show')){
-            hideText_btn.innerHTML = 'Leer menos...';
-        }
-        else{
-            hideText_btn.innerHTML = 'Leer más...';
-        }
-
+        btn.textContent = hideText.classList.contains('show')
+            ? 'Leer menos...'
+            : 'Leer más...';
     });
 
 });
